f7c0120c0c5fac9c9f03ef5bd13ac3d88e6502a5
ikarpiatkowski <ikarpiatkowski@proton.me>
2022-11-19 04:36:16 +0100

    handled product's buy now button

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,7 +11,11 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+    setShowCart(true);
+  };
   return (
     <div>
       <div className="product-detail-container">
@@ -68,10 +72,10 @@ const ProductDetails = ({ product, products }) => {
               className="add-to-cart"
               onClick={() => onAdd(product, qty)}
             >
-              Add to Cart
+              Dodaj do koszyka
             </button>
-            <button type="button" className="buy-now" onClick="">
-              Buy Now
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
+              Kup teraz
             </button>
           </div>
         </div>
